9a39eb7678a2706d4363b4c91e946e22b4278c0d
Chris Clifton <chrisc03@hotmail.com>
2024-10-21 09:47:36 -0400

    update index with bootstrap and move app.py

--- app.py
+++ app.py
@@ -22,3 +22,39 @@ async def read_item(request: Request, id: str):
     return templates.TemplateResponse(
         request=request, name="item.html", context={"id": id, "name": "This is my item"},
     )
+
+@app.get("/manage_drugs", response_class=HTMLResponse)
+async def manage_drugs(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="manage_drugs.html"
+    )
+
+@app.get("/Patient_login", response_class=HTMLResponse)
+async def Patient_login(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="Patient_login.html"
+    )
+
+@app.get("/patient_profile", response_class=HTMLResponse)
+async def patient_profile(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="patient_profile.html"
+    )
+
+@app.get("/ContactUs_Page", response_class=HTMLResponse)
+async def ContactUs_Page(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="ContactUs_Page.html"
+    )
+
+@app.get("/HealthInfo_Page", response_class=HTMLResponse)
+async def HealthInfo_Page(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="HealthInfo_Page.html"
+    )
+
+@app.get("/registration_form", response_class=HTMLResponse)
+async def registration_form(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="registration_form.html"
+    )
